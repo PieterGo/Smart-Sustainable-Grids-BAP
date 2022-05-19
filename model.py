f9dd1ac255a1081fdc2ec1028f68685dc060b386
ricksteman <ricksteman@live.nl>
2022-05-19 21:16:20 +0200

    Auto stash before merge of "Group_C" and "origin/Group_C"

--- model.py
+++ model.py
@@ -14,24 +14,31 @@ import numpy as np
 
 def readInputFile(filename):
     
-    # This method loads all the data from the .xlsx file. You do not need to modify anything here.
+    # Load all data from excel sheets
     
     LoadData = pandas.read_excel(filename, sheet_name= 'Load', index_col=0) #data of load entire neighbourhood
-    WeatherData = pandas.read_excel(filename, sheet_name= 'Weather', index_col=0) #data of the weather
     TransformerData = pandas.read_excel(filename, sheet_name= 'Transformer', index_col=0) #rated power transformer
-    PVSystem = pandas.read_excel(filename, sheet_name='PVSystem', index_col=0) 
+    PVProduction = pandas.read_excel(filename, sheet_name='PVProduction', index_col=0) 
     StorageData = pandas.read_excel(filename, sheet_name= 'StorageSystem', index_col=0) # batterij
     EVDemand= pandas.read_excel(filename, sheet_name = 'EVDemand', index_col=0) #EV charging data
 
-
-    # We could return multiple items but it is preferable to return a structure, in this case a dictionary 
-    return {'WeatherData':WeatherData, 'PVSystem':PVSystem, 'StorageData':StorageData,
+    # Return directory 
+    return {'PVProduction':PVProduction, 'StorageData':StorageData,
             'LoadData':LoadData, 'TransformerData':TransformerData, 'EVDemand':EVDemand}
 
 filename = 'variables_BAP.xlsx'
 data = readInputFile(filename)
 print(data)
 
+def optimizationModel(inputData, modelType):   
+    
+    #Unpack the data from the dictionary
+    
+    LoadData = inputData['LoadData']
+    TransformerData = inputData['TransformerData']
+    PVProduction = inputData['PVProduction']
+    StorageData = inputData['StorageData']
+    EVDemand = inputData['EVDemand']
 
 
 
