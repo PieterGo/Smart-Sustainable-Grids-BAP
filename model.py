14dfca5619436e28126ebeea48655eb4df03b93d
ricksteman <ricksteman@live.nl>
2022-05-19 12:35:41 +0200

    Excel infrastructure finalized

--- model.py
+++ model.py
@@ -16,21 +16,17 @@ def readInputFile(filename):
     
     # This method loads all the data from the .xlsx file. You do not need to modify anything here.
     
+    LoadData = pandas.read_excel(filename, sheet_name= 'Load', index_col=0) #data of load entire neighbourhood
     WeatherData = pandas.read_excel(filename, sheet_name= 'Weather', index_col=0) #data of the weather
-    # SystemDemand= pandas.read_excel(filename, sheet_name = 'SystemDemand', index_col= 0) #user data
-    # EVDemand= pandas.read_excel(filename, sheet_name = 'EVDemand', index_col= 0) #EV charging data
+    TransformerData = pandas.read_excel(filename, sheet_name= 'Transformer', index_col=0) #rated power transformer
     PVSystem = pandas.read_excel(filename, sheet_name='PVSystem', index_col=0) 
-    # LoadData = pandas.read_excel(filename, sheet_name = 'Loads', index_col= 0) # wat moeten we hiermee ivm groep A? @nanda
-    # PVData = pandas.read_excel(filename, sheet_name='PVParks', index_col=0) # wat moeten we hiermee ivm groep A? @nanda
     StorageData = pandas.read_excel(filename, sheet_name= 'StorageSystem', index_col=0) # batterij
-    # UnitData = pandas.read_excel(filename, sheet_name = 'Generators', index_col= 0) # transformer data (bij ons alleen min&max)
+    EVDemand= pandas.read_excel(filename, sheet_name = 'EVDemand', index_col=0) #EV charging data
 
-
-#  'EVDemand':EVDemand,    
 
     # We could return multiple items but it is preferable to return a structure, in this case a dictionary 
-    return {'WeatherData':WeatherData, 'PVSystem':PVSystem, 'StorageData':StorageData}
-            #'SystemDemand':SystemDemand, 'LoadData':LoadData, 'PVData':PVData, 'UnitData':UnitData}
+    return {'WeatherData':WeatherData, 'PVSystem':PVSystem, 'StorageData':StorageData,
+            'LoadData':LoadData, 'TransformerData':TransformerData, 'EVDemand':EVDemand}
 
 filename = 'variables_BAP.xlsx'
 data = readInputFile(filename)
